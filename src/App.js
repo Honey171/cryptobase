3a68627ddcafeaf268b68448e7c64e33df56b314
Honey171 <103601089+Honey171@users.noreply.github.com>
2022-10-29 20:16:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Sigin from './pages/Signin';
+import Signup from './pages/Signup';
+import Account from './pages/Account';
+import CoinPage from './pages/CoinPage';
+import Footer from './components/Footer';
+
+import axios from 'axios';
+import { AuthContextProvider } from './context/AuthContext';
+import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true';
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+    });
+  }, [url]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home coins={coins} />}
+          />
+          <Route
+            path="/signin"
+            element={<Sigin />}
+          />
+          <Route
+            path="/signup"
+            element={<Signup />}
+          />
+          <Route
+            path="/account"
+            element={<Account />}
+          />
+          <Route
+            path="/coin/:coinId"
+            element={<CoinPage />}
+          >
+            <Route path=":coinId" />
+          </Route>
+        </Routes>
+        <Footer />
+      </AuthContextProvider>
+    </ThemeProvider>
   );
 }
 
